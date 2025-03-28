35978be7ee3e1bebad7c13c68018eb3d17c809e0
Abhijae <abhijae@gmail.com>
2025-03-29 02:17:23 +0530

    Fuction to update struct

--- GO_PACKAGE_EXP/mypackage/StructExample.go
+++ GO_PACKAGE_EXP/mypackage/StructExample.go
@@ -4,18 +4,27 @@ import (
 	"fmt"
 )
 
+type Person struct {
+	Name string
+	Age  int
+}
+
 func StructExample() {
 	fmt.Println("Example for struct")
-
-	type Person struct {
-		Name string
-		Age  int
-	}
 
 	p1 := Person{Name: "Shashi", Age: 20}
 
 	// Print struct
 
 	fmt.Println("Name", p1.Name)
+
+	NameChange(&p1, "Saleem")
+
+	fmt.Println("Name", p1.Name)
+
 	fmt.Println("Age", p1.Age)
+}
+
+func NameChange(p *Person, newName string) {
+	p.Name = newName
 }
