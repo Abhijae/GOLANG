64cf35f7c657bf095d7159a5296d703e58e712de
Abhijae <abhijae@gmail.com>
2025-03-28 20:39:49 +0530

    array example

--- GO_PACKAGE_EXP/main.go
+++ GO_PACKAGE_EXP/main.go
@@ -10,4 +10,33 @@ func main() {
 	fmt.Println("Sum:", result)    // Output: Sum: 30
 	fmt.Println("hello:", result)  // Output: Sum: 30
 	fmt.Println("HAMMME:", result) // Output: Sum: 30
+
+	//// Defining an array
+
+	arr := [5]int{1, 2, 3, 4, 5}
+
+	for index, value := range arr {
+		fmt.Println(index, value)
+	}
+
+	// using range without indexing
+
+	for _, value := range arr {
+		fmt.Println(value)
+	}
+
+	// Looping over multi dimensional array
+
+	multiarr := [3][3]int{
+		{1, 2, 3},
+		{2, 4, 6},
+		{3, 6, 9},
+	}
+
+	for _, row := range multiarr {
+		for _, value := range row {
+			fmt.Println(value)
+		}
+	}
+
 }
